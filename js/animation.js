8986c065ce2db77896f3e5b673e9ed15ace0d2f0
J.Pean <astrofphinix@gmail.com>
2024-09-24 00:17:45 +0800

    semi working confetti

--- js/animation.js
+++ js/animation.js
@@ -1,13 +1,31 @@
-var menuBtn = document.getElementById("menuBtn");
-var sideNav = document.getElementById("sideNav");
+// Select all elements with the class 'hideableObject'
+const objects = document.querySelectorAll(".hideableObject");
 
-sideNav.style.right = "-40vw";
+// Loop through each object and attach event listeners
+objects.forEach((object) => {
+  object.addEventListener("click", () => {
+    object.style.display = "none"; // Hide the object on click
+  });
 
-menuBtn.onclick = function () {
-  if (sideNav.style.right == "-140vw") {
-    sideNav.style.right = "0";
-  } else {
-    sideNav.style.right = "-140vw";
-  }
-};
+  object.addEventListener("touchstart", () => {
+    object.style.display = "none"; // Hide the object on touch
+  });
+});
+// document.querySelectorAll(".confetti").forEach((el) => {
+//   el.addEventListener("mouseover", () => {
+//     el.style.opacity = 0;
+//   });
+// });
+// Select all elements with the class 'hideableObject'
+// const objects = document.querySelectorAll(".hideableObject");
 
+// // Loop through each object and attach event listeners
+// objects.forEach((object) => {
+//   object.addEventListener("click", () => {
+//     object.style.display = "none"; // Hide the object on click
+//   });
+
+//   object.addEventListener("touchstart", () => {
+//     object.style.display = "none"; // Hide the object on touch
+//   });
+// });
